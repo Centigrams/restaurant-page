7c19fdbc574bfa54efa04204feddffdfa479c624
Sidd Bairan <bairansa@gmail.com>
2021-08-27 15:55:27 +0800

    Add restaurant name

---
+++ src/home.js
@@ -0,0 +1,41 @@
+const createText = (text, tag, idName, className) => {
+  const textElement = document.createElement(tag);
+  textElement.textContent = text;
+  textElement.setAttribute('id', idName);
+  textElement.classList.add(className);
+  return textElement;
+};
+
+const renderHome = () => {
+  const homeDiv = document.createElement('div');
+  homeDiv.setAttribute('id', 'home-div');
+
+  homeDiv.appendChild(
+    createText(
+      'Ginza Kyu', 
+      'h1', 
+      'restaurant-name', 
+      null
+    )
+  );
+
+  homeDiv.appendChild(
+    createText(
+      'World-class. Since 1935.',
+      'p',
+      'restaurant-line',
+      null
+    )
+  );
+
+  return homeDiv;
+};
+
+const initiateHome = () => {
+  const main = document.getElementById('main');
+  //? Is this enough to clear page?
+  main.textContent = "";
+  main.appendChild(renderHome());
+};
+
+export default initiateHome;
